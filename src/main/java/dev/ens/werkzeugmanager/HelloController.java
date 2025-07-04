41361fc52c73d8c023d2e4c9c6b9738a037154dc
DerEns86 <v.ens86@yahoo.de>
2025-07-04 21:36:53 +0200

    update UI to display tool and machine details

--- src/main/java/dev/ens/werkzeugmanager/HelloController.java
+++ src/main/java/dev/ens/werkzeugmanager/HelloController.java
@@ -1,18 +1,26 @@
 package dev.ens.werkzeugmanager;
 
+import dev.ens.werkzeugmanager.model.Machine;
 import dev.ens.werkzeugmanager.model.Tool;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label werkzeugText;
+    @FXML
+    private Label maschinenText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onWerkzeugButtonClick() {
         Tool fraeser = new Tool("1", "Fräser1", "Goedde", "Fräser", 4.0, 4);
-        welcomeText.setText(fraeser.getDetails());
+        werkzeugText.setText(fraeser.getDetails());
 
+    }
 
+    @FXML
+    protected void onMaschinenButtonClick() {
+        Machine maschine1 = new Machine("1","Multus B300", "Okuma", "Mill-Turn", 50, "Werk 1");
+        maschinenText.setText(maschine1.getDetails());
     }
 }
